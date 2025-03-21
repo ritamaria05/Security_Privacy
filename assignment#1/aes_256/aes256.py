5f74f19d77fba1dc89f0c8c484f0f8c6aec24204
Maximiliano Sá <max.phillips.sa@gmail.com>
2025-03-21 13:19:05 +0000

    update

--- assignment#1/aes_256/aes256.py
+++ assignment#1/aes_256/aes256.py
@@ -1,6 +1,20 @@
 #-- in progress - Max :) --#
 from cryptography.hazmat.primitives.ciphers import Cipher,algorithms,modes
+from os import urandom
+import timeit
+
+key = urandom(32) # key of 32 bytes (256 bits)
+iv = urandom(16) # iv has to be 16 bytes
 
 def encrypt():
     return
 
+
+
+def main():
+    print("test")
+    return
+
+
+if __name__ == "__main__":
+    main()
